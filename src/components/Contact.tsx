5fb65aeb609537b8ccf03ff3d268355baaa4b94f
Hamad Sedrati <125213206+hamadsedrati@users.noreply.github.com>
2026-05-28 15:58:15 +0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -52,7 +52,7 @@ const Contact: React.FC = () => {
   const socialLinks = [
     { icon: Github, href: 'https://github.com/hamadsedrati', label: 'GitHub' },
     { icon: Linkedin, href: 'https://www.linkedin.com/in/hamad-sedrati/', label: 'LinkedIn' },
-    { icon: Tryhackme, href: 'https://twitter.com', label: 'Twitter' },
+    { icon: Twitter, href: 'https://twitter.com', label: 'Twitter' },
   ];
 
   return (
